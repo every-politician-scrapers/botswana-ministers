d03634ddcd2dfcb4f4ee5f5e99c6a817f30484f4
Tony Bowden <tony@tmtm.com>
2022-07-13 16:53:05 +0300

    add official source for last reshuffle
    This is a static source, so we don't want to keep fetching and scraping
    it.

--- sources/official/compare.sh
+++ sources/official/compare.sh
@@ -2,8 +2,9 @@
 
 cd $(dirname $0)
 
-# bundle exec ruby scraper.rb > scraped.csv
-# wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid > wikidata.csv
-# bundle exec ruby diff.rb | tee diff.csv
+# scraped.csv generated manually from source
+
+wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid > wikidata.csv
+bundle exec ruby diff.rb | qsv sort -s itemlabel | tee diff.csv
 
 cd ~-
